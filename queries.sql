e38e28cf30efb356dec6b29b2a439a8c2fe2b7b7
FrankMugagga <frank.mugagga@gmail.com>
2023-05-24 18:27:12 +0300

    Add commands to query data the animals table

--- queries.sql
+++ queries.sql
@@ -8,3 +8,30 @@ SELECT name, escape_attempts FROM animals WHERE (weight_kg > 10.5);
 SELECT * FROM animals WHERE neutered IN (true);
 SELECT * FROM animals WHERE name not IN ('Gabumon');
 SELECT * FROM animals WHERE weight_kg IN (10.4, 17.3) OR (weight_kg > 10.4 AND weight_kg < 17.3);
+
+BEGIN;
+SAVEPOINT SP1;
+ALTER TABLE animals RENAME COLUMN species TO unspecified;
+ROLLBACK TO SP1;
+UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals SET species = 'Pokemon' WHERE name NOT LIKE '%mon';
+COMMIT;
+
+BEGIN;
+SAVEPOINT SP2;
+DELETE FROM animals;
+ROLLBACK TO SP2;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+
+SAVEPOINT SP3;
+UPDATE animals SET weight_kg = weight_kg * -1;
+ROLLBACK TO SP3;
+SELECT COUNT(name) from animals;
+SELECT * FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, MAX(escape_attempts) FROM animals GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+
+
+
+
