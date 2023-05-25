105a28ffea38594cd52cf8b0685c927068a9c43f
FrankMugagga <frank.mugagga@gmail.com>
2023-05-25 11:34:16 +0300

    Add the SQL commands after instruction execution

--- data.sql
+++ data.sql
@@ -39,3 +39,15 @@ INSERT INTO animals(
     VALUES ('Boarmon', '2005-06-07', 20.4, true, 7), 
     ('Blossom', '1998-10-13', 17, true, 3), 
     ('Ditto', '2022-05-14', 22, true, 4);
+
+/* Vet clinic database: query multiple tables */
+INSERT INTO owners (full_name, age) VALUES ('Sam Smith', 34), ('Jennifer Orwell',19 ), ('Bob', 45 ), ('Melody Pond', 77), ('Dean Winchester', 14), ('Jodie Whittaker', 38);
+INSERT INTO species (name) VALUES ('Pokemon'), ('Digimon');
+UPDATE animals SET species_id = 2 WHERE name LIKE'%mon';
+UPDATE animals SET species_id = 1 WHERE name NOT LIKE'%mon';
+UPDATE animals SET owner_id = 1 WHERE name = 'Agumon';
+UPDATE animals SET owner_id = 2 WHERE name = 'Gabumon';
+UPDATE animals SET owner_id = 2 WHERE name = 'Pikachu';
+UPDATE animals SET owner_id = 3 WHERE name IN ('Devimon', 'Plantmon');
+UPDATE animals SET owner_id = 4  WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+UPDATE animals SET owner_id = 5  WHERE name IN ('Angemon', 'Boarmon');
--- schema.sql
+++ schema.sql
@@ -7,3 +7,12 @@ CREATE TABLE animals (
 );
 ALTER TABLE animals add species varchar(50);
 
+/* Vet clinic database: query multiple tables */
+CREATE TABLE owners(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, full_name VARCHAR(250), age int);
+CREATE TABLE species(id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, name VARCHAR(100));
+ALTER TABLE animals ADD PRIMARY KEY(id);
+ALTER TABLE animals DROP species;
+ALTER TABLE animals ADD species_id int;
+ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE CASCADE;
+ALTER TABLE animals ADD owner_id int;
+ALTER TABLE animals ADD CONSTRAINT fk_owner FOREIGN KEY(owner_id) REFERENCES owners(id) ON DELETE CASCADE;
