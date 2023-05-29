85b73744ad7f9bccde286a64fda374df015b226a
FrankMugagga <frank.mugagga@gmail.com>
2023-05-29 20:15:33 +0300

    Add commands that were executed

--- data.sql
+++ data.sql
@@ -63,3 +63,11 @@ INSERT INTO VISITS (ANIMALS_ID, VETS_ID, DATE_OF_VISIT) VALUES(1, 1, '2020-05-24
 ), (6, 2, '2021-04-07'), (7, 3, '2019-09-29'), (1, 4, '2020-10-03'), (1, 4, '2020-11-04'), (10, 1, '2021-01-11'), (10, 3, '2020-05-24');
 INSERT INTO VISITS (ANIMALS_ID, VETS_ID, DATE_OF_VISIT) VALUES(9, 2, '2019-01-24'), (9, 2, '2020-02-27'), (9, 2, '2020-08-03');
  INSERT INTO VISITS (ANIMALS_ID, VETS_ID, DATE_OF_VISIT) VALUES(9, 2, '2019-05-15');
+
+ /* Vet clinic database: database performance audit */
+ 
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
